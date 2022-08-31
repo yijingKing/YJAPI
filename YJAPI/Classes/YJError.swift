f5471257484380eabb4bbf9861c73cd520b35436
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2022-08-31 16:45:54 +0800

    no message

--- YJAPI/Classes/YJError.swift
+++ YJAPI/Classes/YJError.swift
@@ -1,10 +1,10 @@
-//
-//  File.swift
-//  YJAPI_Example
-//
-//  Created by 祎 on 2022/8/30.
-//  Copyright © 2022 CocoaPods. All rights reserved.
-//
+/*******************************************************************************
+Copyright (K), 2020 - ~, ╰莪呮想好好宠Nǐつ
+
+Author:        ╰莪呮想好好宠Nǐつ
+E-mail:        1091676312@qq.com
+GitHub:        https://github.com/yijingKing
+********************************************************************************/
 
 import Foundation
 import Alamofire
